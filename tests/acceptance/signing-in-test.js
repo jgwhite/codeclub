691bf83ae3682a1fb663d47fd26dd3ce6bf43587
Jamie White <jamie@jgwhite.co.uk>
2015-03-22 11:08:33 +0000

    Add simple acceptance test

---
+++ tests/acceptance/signing-in-test.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+import {
+  module,
+  test
+} from 'qunit';
+import startApp from 'codeclub/tests/helpers/start-app';
+
+var application;
+
+module('Acceptance: Signing in', {
+  beforeEach: function() {
+    application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(application, 'destroy');
+  }
+});
+
+test('succesfully', function(assert) {
+  visit('/');
+  fillIn('input[name="email"]', 'hello@example.com');
+  fillIn('input[name="password"]', 'password');
+  click('button:contains("Log in")');
+  andThen(() => {
+    assert.ok(find(':contains("Logged in")').length,
+      'expected to see "Logged in"');
+  });
+});
