f3d416567ac9249563a8d3626cafe48ac87f86e8
Jamie White <jamie@jgwhite.co.uk>
2015-03-22 22:13:53 +0000

    Space out firebase initializer

--- app/initializers/firebase.js
+++ app/initializers/firebase.js
@@ -4,6 +4,7 @@ export function initialize(container, application) {
   let firebase = application.firebase || new Firebase(config.firebase);
 
   application.register('service:firebase', firebase, { instantiate: false });
+
   application.inject('route', 'firebase', 'service:firebase');
   application.inject('adapter', 'firebase', 'service:firebase');
 }
